df358670370f810756de63c1c753cc72f4ab0e97
RobinSuthar <robinsuthar.rs07@gmail.com>
2026-02-18 23:41:15 -0700

    added_database

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Hello Docs Page</div>;
+import { prisma } from "@repo/db";
+
+export default async function Home() {
+  const user = await prisma.user.findFirst();
+  return (
+    <div>
+      Hello Docs Page
+      <div>{user?.email}</div>
+    </div>
+  );
 }
